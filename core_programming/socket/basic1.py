4c9aa0ab05d508d6b4064e3f3ad0b0ae7776719c
fyyc1204 <kaibinzhao@163.com>
2018-04-03 22:41:48 +0800

    socket

---
+++ core_programming/socket/basic1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import socket
+#from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 36000
+BUFFSIZE = 1024
+ADDR = (HOST,PORT)
+
+TcpSerSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+TcpSerSock.bind(ADDR)
+TcpSerSock.listen(5)
+
+
+while True:
+    print("waiting for connecting ......")
+    TcpClientSock , addr = TcpSerSock.accept()
+    print('...connect from:' , addr )
+
+    while True:
+        data = TcpClientSock.recv(BUFFSIZE)
+        if not data:
+            break
+        TcpClientSock.send( b'%s' %  data)
+    TcpClientSock.close()
+TcpSerSock.close()
